f47e6991c8e50f82e728544ec2796cd99d9a26d9
M Miller <miller.2676@osu.edu>
2019-02-04 13:07:00 -0800

    add ability to change tip percentage

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 import Hello from './Hello';
 
@@ -8,13 +8,24 @@ export default class App extends React.Component {
     super();
     this.state = {
       inputValue: '',
+      tipFactor: '',
+    }
+  }
+
+  updateCustomTip(customTip) {
+    if (customTip) {
+      this.setState({
+        tipFactor: parseFloat(customTip) / 100,
+      })
+    } else {
+      this.setState({tipFactor: 0})
     }
   }
 
   render() {
     let tip = 0.00;
     if (this.state.inputValue) {
-      tip = parseFloat(this.state.inputValue) * 0.2;
+      tip = parseFloat(this.state.inputValue) * this.state.tipFactor;
       tip = (Math.round(tip * 100) / 100).toFixed(2);
     }
 
@@ -28,8 +39,29 @@ export default class App extends React.Component {
           style={styles.input}
           keyboardType='numeric'
           placeholder='0.00'
-          onChangeText={(text)=> this.setState({inputValue: text})}
+          onChangeText={(text)=> this.setState({ inputValue: text })}
         />
+      <View style={styles.buttonGroup}>
+          <Button
+            title='10%'
+            onPress={() => this.setState({ tipFactor: 0.1 })}
+          />
+          <Button
+            title='15%'
+            onPress={() => this.setState({ tipFactor: 0.15 })}
+          />
+          <Button
+            title='20%'
+            onPress={() => this.setState({ tipFactor: 0.2 })}
+          />
+          <TextInput
+            value={(this.state.tipFactor * 100).toString()}
+            style={styles.customTip}
+            keyboardType='numeric'
+            placeholder='25%'
+            onChangeText={customTip => this.updateCustomTip(customTip)}
+          />
+        </View>
       </View>
     );
   }
@@ -43,11 +75,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20,
   },
+  customTip: {
+    height: 40,
+    width: 60,
+    borderColor: '#333',
+    borderWidth: 1,
+    padding: 5,
+  },
   input: {
     height: 40,
     width: '100%',
     borderColor: '#333',
     borderWidth: 1,
     padding: 5,
+  },
+  buttonGroup: {
+    flexDirection: 'row',
   }
+
 });

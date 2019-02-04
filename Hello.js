50bffb7d74501dfe06783f954d8bf090c5423900
M Miller <miller.2676@osu.edu>
2019-02-03 20:20:51 -0800

    initial commit

---
+++ Hello.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, } from 'react-native';
+
+export default class Hello extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.hello}>Hello from Hello.js</Text>
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  hello: {
+    backgroundColor: "#bb0000",
+    color: "white",
+    padding: 10,
+  }
+})
